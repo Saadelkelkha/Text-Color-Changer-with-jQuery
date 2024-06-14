bb47b482b6a6132e8515fb123fcde2716a499de4
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2024-06-14 19:25:34 +0100

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#redButton").click(function() {
+        $("#text").css("color","red");
+    })
+    $("#greenButton").click(function() {
+        $("#text").css("color","green");
+    })
+    $("#blueButton").click(function() {
+        $("#text").css("color","blue");
+    })
+})
